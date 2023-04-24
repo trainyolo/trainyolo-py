3bdc2bd6c399a2d8e6fff2d03e271b985b126515
Davy Neven <neven.davy@gmail.com>
2023-04-24 14:47:49 +0200

    ocr fix

--- trainyolo/utils/ocr.py
+++ trainyolo/utils/ocr.py
@@ -1,13 +1,18 @@
 from trainyolo.client import OCRResult
+import re
 
 def read_f1_conf(client, f1_curve):
     ocr_result = OCRResult.create(client, f1_curve)
     status, result = ocr_result.get_result()
     if status == 'SUCCEEDED':
-        conf = float(result.rsplit('all classes', 1)[1].rsplit('at', 1)[1])
-        if conf < 1 and conf > 0:
-            return conf
-        else:
-            return -1
+        pattern = re.compile(r'all\s+classes\s+(\d+\.\d+)\s+at\s+(\d+\.\d+)')
+        match = pattern.search(result)
+        if match:
+            score, conf = match.groups()
+            conf = float(conf)
+            if conf < 1 and conf > 0:
+                return conf
+            else:
+                return -1
     else:
         return -1
